b00fb40a3be5c510330365cc25e62c5335efccb2
Leo <LAFrilot@outlook.com>
2025-02-23 01:52:46 -0800

    Pushing build stuff

---
+++ Source/UCGameJam/UCGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UCGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCGAMEJAM_API AUCGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
